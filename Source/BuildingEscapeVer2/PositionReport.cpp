e18cc7d5b54058ee9d24306b0cab4aa32bd9a649
Isfandyar Ali <isfandyar.ali@me.com>
2019-03-13 13:31:01 +0000

    added fstring printout of object name

--- Source/BuildingEscapeVer2/PositionReport.cpp
+++ Source/BuildingEscapeVer2/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h" //getOwner autocomplete
 
 // Sets default values for this component's properties/ CONSTRUCTOR!!
 UPositionReport::UPositionReport()
@@ -8,7 +9,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 
 	// ...
 }
@@ -18,9 +18,10 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
- 
 	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position Report reporting duty on %s!"), *ObjectName);
+
 }
 
 
